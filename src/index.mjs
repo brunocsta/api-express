1a134de7436f5cafb91ac5a095e2b017cbb17a06
Bruno Costa <b.costamartins@gmail.com>
2024-08-04 06:21:38 -0300

    Create the index route files

---
+++ src/index.mjs
@@ -0,0 +1,119 @@
+import express from "express";
+import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
+import passport from "passport";
+import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
+//import "./strategies/local-strategy.mjs";
+import "./strategies/discord-strategy.mjs";
+
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/api_express")
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.log(`Error: ${err}`));
+
+app.use(express.json());
+app.use(cookieParser("helloworld"));
+//session middleware antes do passport
+app.use(
+  session({
+    secret: "bruno dev",
+    saveUninitialized: true, //não salva dados nao modificados no sessions store, agora com a base Session Store feita deixo como true
+    resave: false, //o resave substitui o cookie anterior, evitando a o acumulo de logs salvos para a mesma session
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+    store: MongoStore.create({ client: mongoose.connection.getClient() }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(routes);
+
+const PORT = process.env.PORT || 3000; //caso não tenha um port do enviroment será setado 3000
+
+app.post("/api/auth", passport.authenticate("local"), (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  console.log("Inside /auth/status endpoint");
+  console.log(req.user);
+  console.log(req.session);
+  console.log(req.sessionID);
+  return req.user ? res.send(req.user) : res.sendStatus(401);
+});
+
+app.get("/api/auth/discord", passport.authenticate("discord"));
+
+app.get(
+  "/api/auth/discord/redirect",
+  passport.authenticate("discord"),
+  (req, res) => {
+    console.log(req.session);
+    console.log(req.user);
+    res.sendStatus(200);
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Runing on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  console.log(req.session);
+  console.log(req.session.id);
+  req.session.visted = true;
+  res.cookie("hello", "world", { maxAge: 10000, signed: true });
+  res.status(201).send({ msg: "Hello" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "BAD CREDENTIALS" });
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  req.logout((err) => {
+    if (err) return res.sendStatus(400);
+    res.sendStatus(200);
+  });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  const { body: item } = req;
+
+  const { cart } = req.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  return res.send(req.session.cart ?? []);
+});
